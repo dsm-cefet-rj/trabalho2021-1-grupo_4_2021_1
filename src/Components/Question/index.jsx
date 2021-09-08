cdb115467685e5f18a6d6ad036ddaa4e78d9d361
Leonardo Sauberman <leosauberman@gmail.com>
2021-09-08 16:46:19 -0300

    Fix Login, melhora questoes

--- src/Components/Question/index.jsx
+++ src/Components/Question/index.jsx
@@ -1,18 +1,31 @@
 import { TipoEnum } from '../../shared/enums';
+import { useState } from 'react';
 import './styles.css';
 
 export function Question({ dados, isEditing, children }) {
 
-  return (
-    <div className="card col-xs-12 col-sm-10 col-md-8 col-lg-6 col-xl-6">
-      <h4>{`Questão ${children}`}</h4>
-      <textarea rows="3" placeholder="Enunciado" defaultValue={dados.pergunta} disabled={!isEditing}></textarea>
-      {selectTypeOfAnswer(dados, isEditing)}
-    </div>
-  );
-}
+  const [answer, setAnswer] = useState([]);
 
-function selectTypeOfAnswer(dados, isEditing) {
+  const handleAnswerQuestion = (event) => {
+    if(dados.tipo === TipoEnum.multipla) {
+      if(event.target.checked)
+        setAnswer([...answer, event.target.name])
+      else {
+        const newAnswer = [...answer].filter(a => a !== event.target.name);
+        setAnswer(newAnswer);
+      }
+    }
+    else if(dados.tipo === TipoEnum.objetiva) {
+      if(event.target.checked)
+        setAnswer([event.target.name]);
+    }
+    else {
+      setAnswer([event.target.value]);
+    }
+    onAnswer(answer);
+  }
+  
+  function selectTypeOfAnswer(dados, isEditing) {
     switch (dados.tipo) {
       case TipoEnum.discursiva:
         const temResposta = dados.resposta;
@@ -20,6 +33,7 @@ function selectTypeOfAnswer(dados, isEditing) {
           <>
             <textarea rows="3" className={`resposta${temResposta ? '-certa' : ''}`} 
               readOnly={isEditing} disabled={isEditing}
+              onChange={handleAnswer}
               defaultValue={dados.resposta}>
             </textarea>
           </>
@@ -34,7 +48,7 @@ function selectTypeOfAnswer(dados, isEditing) {
                     const res = indice === dados.resposta;
                     return (
                       <label key={indice} className={res ? 'resposta-certa' : ''}>
-                        <input type="radio" name="optradio" defaultChecked={res} />
+                        <input type="radio" name={indice} defaultChecked={res} onChange={handleAnswer} />
                         {valor}
                       </label>
                     );
@@ -53,7 +67,7 @@ function selectTypeOfAnswer(dados, isEditing) {
                     const res = indice === dados.resposta;
                     return (
                       <label key={indice} className={res ? 'resposta-certa' : ''}>
-                        <input type="checkbox" defaultValue="" defaultChecked={res} /> {valor}
+                        <input type="checkbox" name={indice} defaultValue="" defaultChecked={res} onChange={handleAnswer} /> {valor}
                       </label>
                     )
                   })
@@ -61,8 +75,18 @@ function selectTypeOfAnswer(dados, isEditing) {
               </div>
             );
         }
-        return;         
+        return;
       default:
         return;
     }
   }
+
+  return (
+    <div className="card col-xs-12 col-sm-10 col-md-8 col-lg-6 col-xl-6">
+      <h4>{`Questão ${children}`}</h4>
+      <p>{dados.pergunta}</p>
+      {selectTypeOfAnswer(dados, isEditing)}
+    </div>
+  );
+}
+
