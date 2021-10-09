d254c092bea7a8e73e71da69a04d38a7e70ff3dc
Eduardo Tadashi <quadros.tadashi@gmail.com>
2021-10-09 19:51:05 -0300

    comentário escola

--- back-end/app.js
+++ back-end/app.js
@@ -84,6 +84,7 @@ app.use('/exames', verifyJWT, exames);
 app.use('/turmas', verifyJWT, turmas);
 app.use('/professores', verifyJWT, professores);
 app.use('/alunos', verifyJWT, alunos)
+// essa entidade não vai ser implementada por enquanto
 app.use('/escola', verifyJWT, escola);
 app.use('/questoes', verifyJWT, escola);
 app.use('/respostas', verifyJWT, respostas);
